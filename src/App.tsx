d710efabe5ab02906be9b302b52cfb9979d701f2
Felipe Chernicharo <string7dev@gmail.com>
2021-02-22 14:30:12 -0300

    experienece bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
+import ExperienceBar from "./components/ExperienceBar";
 import "./styles/global.css";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello NLW</h1>
-    </div>
+    <>
+      <div className="container">
+        <ExperienceBar />
+      </div>
+
+      {/* <button style={{ height: "3rem" }}>Button</button> */}
+      {/* <h1>Hello NLW</h1>  */}
+    </>
   );
 }
 

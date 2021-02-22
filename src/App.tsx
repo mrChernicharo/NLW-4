5629564e6f8dcaede5939c8b9ad7a586a86c1396
Felipe Chernicharo <string7dev@gmail.com>
2021-02-22 14:12:21 -0300

    variaveis globais

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
-import Button from "./components/Button";
 import "./styles/global.css";
 
 function App() {
   return (
     <div className="App">
       <h1>Hello NLW</h1>
-      <Button color="blue">Azul</Button>
-      <Button color="orangered">Laranja</Button>
-      <Button color="green">Verde</Button>
     </div>
   );
 }
